54cbb4627232b10d5c59e8e7a04dd726b1b5f51b
KKMDB17 <officepk109@gmail.com>
2025-06-09 12:41:54 +0500

    Fetch User Details API

---
+++ config_2.py
@@ -0,0 +1,10 @@
+class fetch_user_details_variables:
+    valid_url="http://localhost:5000/api/user_details/2009"
+    invalid_url="http://localhost:5000/api/user_detailz/2009"
+    invalid_user="http://localhost:5000/api/user_details/200929665"
+    valid_headers={
+        "Accept":"application/json"
+    }
+    invalid_headers={
+        "Accept":"application/jsons"
+    }
---
+++ test_fetch_user_details_api.py
@@ -0,0 +1,35 @@
+import json
+import pytest
+import requests
+from config_2 import fetch_user_details_variables as fdapi
+######################Valid_Case#######################################
+def test_case_1_valid_request():
+    case_1 = requests.get(fdapi.valid_url,headers=fdapi.valid_headers)
+    assert case_1.status_code == 200,"Valid Request Failed"
+######################Invalid_URL#####################################
+def test_case_2_invalid_url():
+    case_2 = requests.post(fdapi.invalid_url,headers=fdapi.valid_headers)
+    assert case_2.status_code == 404, "API Request Failed on Invalid URL"
+#######################API_Methods###################################
+def test_case_3_post_method():
+    case_3 = requests.post(fdapi.valid_url,headers=fdapi.valid_headers)
+    assert case_3.status_code == 405, "API Request Failed on Post Method"
+def test_case_4_put_method():
+    case_4 = requests.put(fdapi.valid_url,headers=fdapi.valid_headers)
+    assert case_4.status_code == 405,"API Request Failed on Put Method"
+def test_case_5_patch_method():
+    case_5 = requests.patch(fdapi.valid_url,headers=fdapi.valid_headers)
+    assert case_5.status_code == 405, "API Request Failed on Patch Method"
+def test_case_6_delete_method():
+    case_6 = requests.delete(fdapi.valid_url,headers=fdapi.valid_headers)
+    assert case_6.status_code == 405,"API Request Failed on Delete Method"
+########################Invalid_User_ID##################################
+def test_case_7_invalid_user_id():
+    case_7 = requests.post(fdapi.valid_url,headers=fdapi.valid_headers)
+    assert case_7.status_code == 404, "API Request Failed on Invalid User ID Search"
+######################Invalid_Format#######################################
+def test_case_8_invalid_format():
+    case_8 = requests.get(fdapi.valid_url,headers=fdapi.invalid_headers)
+    print(f"Response Message: {case_8.json().get('error')}")
+    assert case_8.status_code == 406,"API Request Failed on Invalid Format"
+    
